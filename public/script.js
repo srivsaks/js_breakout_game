f7898707563c6588df51021f4f84afce3a37de45
Sakshi Srivastava <srivsaks@amazon.com>
2022-02-24 19:21:17 +0530

    Create the bricks

--- public/script.js
+++ public/script.js
@@ -7,6 +7,9 @@ const canvas=document.getElementById("canvas");
 const ctx=canvas.getContext("2d");
 let score =0;
 
+const brickRowCount=9;
+const brickColCount=5;
+
 // create ball props
 const ball = {
     x: canvas.width/2,
@@ -25,6 +28,27 @@ w:80,
 h:10,
 speed:8,
 dx:0
+}
+
+// create brick props
+const brickInfo={
+w:70,
+h:20,
+padding:10,
+offSetX:45,
+offSetY:60,
+visible:true
+}
+
+const bricks=[];
+
+for(let i=0;i<brickRowCount;i++){
+bricks[i]=[];
+for(let j=0;j<brickColCount;j++){
+    const x=i*(brickInfo.w+brickInfo.padding)+brickInfo.offSetX;
+    const y=j*(brickInfo.h+brickInfo.padding)+brickInfo.offSetY;
+    bricks[i][j]={x,y,...brickInfo}
+}
 }
 
 // draw ball on canvas
@@ -51,11 +75,25 @@ function drawScore(){
  ctx.fillText(`Score: ${score}`,canvas.width-100,30)
 }
 
+//draw bricks
+function drawBricks(){
+bricks.forEach((col)=>{
+    col.forEach((brick)=>{
+        ctx.beginPath();
+        ctx.rect(brick.x,brick.y,brick.w,brick.h);
+        ctx.fillStyle=brick.visible?"#0095dd":"transparent";
+        ctx.fill();
+        ctx.closePath();
+    })
+})
+}
+
 //draw everything
 function draw(){
     drawBall();
     drawPaddle();
     drawScore();
+    drawBricks();
 }
 
 draw();
